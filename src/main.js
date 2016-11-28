22c7179f53c2055f679fd7031cbfa973aacf4255
rwoll <ross.wollman@gmail.com>
2016-11-28 00:05:32 -0800

    Update app to be a Menubar application

--- src/main.js
+++ src/main.js
@@ -1,80 +1,83 @@
-import electron, { app, BrowserWindow, globalShortcut, ipcMain, Menu } from 'electron';
+import { app, globalShortcut, ipcMain, Menu, dialog } from 'electron';
 import path from 'path';
 import url from 'url';
 import config from './config'
 import { execFile } from 'child_process';
+import Menubar from 'menubar';
 
-let mainWindow;
-
-function createWindow () {
-  mainWindow = new BrowserWindow({
+if (process.platform !== 'darwin') {
+  dialog.showErrorBox(
+    'Incompatible OS',
+    [`Your operating system (${process.platform}) is not supported at this time.`,
+     'This application will now quit.',
+     'Have a nice day!'].join('\n\n')
+  );
+  app.quit();
+} else {
+  let mb = new Menubar({
+    index: url.format({
+      pathname: path.join(__dirname, 'ui', 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }),
+    preloadWindow: true,
     width: config.WINDOW_WIDTH,
     height: config.WINDOW_HEIGHT,
-    alwaysOnTop: true,
-    frame: false,
-    maximizable: false,
-    fullscreenable: false,
-    resizable: false,
-    show: false
+    windowPosition: 'center'
   });
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'ui', 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+  mb.on('ready', () => {
+    let menu = Menu.buildFromTemplate([{
+      label: 'SpotIt',
+      submenu: [{
+        label: 'Quit Application',
+        accelerator: 'Command+Q',
+        selector: 'terminate:'
+      }]
+    }]);
 
-  mainWindow.webContents.on('did-finish-load', function() {
-    mainWindow.show();
-  });
+    Menu.setApplicationMenu(menu)
 
-  mainWindow.on('blur', function() {
-    Menu.sendActionToFirstResponder('hide:');
-  });
+    const ret = globalShortcut.register(config.QUICK_CMD, () => {
+      mb.window.isVisible() ? mb.window.hide() : mb.window.show();
+    });
 
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-
-  const ret = globalShortcut.register(config.QUICK_CMD, () => {
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      mainWindow.show();
+    if (!ret) {
+      console.log('registration failed')
     }
-  })
 
-  if (!ret) {
-    console.log('registration failed')
-  }
+    ipcMain.on('asynchronous-message', (event, arg) => {
+      switch (arg.type) {
+        case 'search-results':
+          mb.window.setSize(
+            config.WINDOW_WIDTH,
+            config.WINDOW_HEIGHT + config.WINDOW_HEIGHT * arg.qty
+          );
+          break;
+        case 'search-selected':
+          execFile(path.join(__dirname, 'play.AppleScript'), [arg.id], (err, stdout, stderr) => {});
+          event.sender.send('asynchronous-reply', {type: 'search-reset'});
+          mb.window.hide();
+          break;
+        case 'search-abort':
+          event.sender.send('asynchronous-reply', {type: 'search-reset'});
+          mb.window.hide();
+          break;
+        default:
+          console.warn(`Unknown asynchronous-message received: ${arg.type}`);
+          event.sender.send('asynchronous-reply', {type: 'search-reset'});
+          mb.window.hide();
 
-  ipcMain.on('asynchronous-message', (event, arg) => {
-    if (arg.type === 'search-results') {
-      mainWindow.setSize(
-        config.WINDOW_WIDTH,
-        config.WINDOW_HEIGHT + config.WINDOW_HEIGHT * arg.qty
-      );
-    } else if (arg.type === 'search-selected') {
-      execFile(path.join(__dirname, 'play.AppleScript'), [arg.id], (err, stdout, stderr) => {});
-      event.sender.send('asynchronous-reply', {type: 'search-reset'});
-      Menu.sendActionToFirstResponder('hide:');
-    } else if (arg.type === 'search-abort') {
-      event.sender.send('asynchronous-reply', {type: 'search-reset'});
-      Menu.sendActionToFirstResponder('hide:');
-    }
+      }
+    });
+
+    mb.window.once('ready-to-show', () => {
+      mb.window.show();
+    });
+
+    app.on('will-quit', () => {
+      globalShortcut.unregister(config.QUICK_CMD);
+      globalShortcut.unregisterAll();
+    });
   });
 }
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
